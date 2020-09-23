6f1abf1815807dfcb9adfa46f298ec6745c2e025
Clark C. Evans <cce@clarkevans.com>
2020-09-23 09:41:21 -0500

    adding htmx.org bootstrap example

--- src/TodoMVC.jl
+++ src/TodoMVC.jl
@@ -1,5 +1,25 @@
 module TodoMVC
 
-greet() = print("Hello World!")
+using HTTP
 
-end # module
+homepage(req::HTTP.Request) =
+    HTTP.Response(200,"""
+<html>
+  <body>
+    <script src="https://unpkg.com/htmx.org@0.1.2"></script>
+    <button hx-post="/clicked" hx-swap="outerHTML">Click Me</button>
+  </body>
+</html>
+""")
+
+clicked(req::HTTP.Request) = HTTP.Response(200,"<div>I'm clicked!</div>")
+
+const ROUTER = HTTP.Router()
+HTTP.@register(ROUTER, "GET", "/", homepage)
+HTTP.@register(ROUTER, "POST", "/clicked", clicked)
+
+function serve()
+    HTTP.serve(ROUTER, "127.0.0.1", 8080)
+end
+
+end
