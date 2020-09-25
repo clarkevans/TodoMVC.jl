454da476e4369549b9c09aaade2a42501cce60a9
Clark C. Evans <cce@clarkevans.com>
2020-09-25 14:43:59 -0500

    adding support for Hyperscript.jl server-side DOM

--- src/TodoMVC.jl
+++ src/TodoMVC.jl
@@ -1,18 +1,21 @@
 module TodoMVC
 
 using HTTP
+import HTTP: Response
+using Hyperscript
+@tags html body script button div
+
+Response(status::Int, node::Hyperscript.Node) = Response(status,
+  [SubString("Content-Type") => SubString("text/html; charset=utf-8")];
+  body=HTTP.bytes(string(node)))
+Response(node::Hyperscript.Node) = Response(200, node)
 
 homepage(req::HTTP.Request) =
-    HTTP.Response(200,"""
-<html>
-  <body>
-    <script src="https://unpkg.com/htmx.org@0.1.2"></script>
-    <button hx-post="/clicked" hx-swap="outerHTML">Click Me</button>
-  </body>
-</html>
-""")
+    HTTP.Response(200, html(body(
+     script(src="https://unpkg.com/htmx.org@0.1.2"),
+     button(hxPost="/clicked", hxSwap="outerHTML", "Click Me"))))
 
-clicked(req::HTTP.Request) = HTTP.Response(200,"<div>I'm clicked!</div>")
+clicked(req::HTTP.Request) = Response(div("I'm clicked"))
 
 const ROUTER = HTTP.Router()
 HTTP.@register(ROUTER, "GET", "/", homepage)
